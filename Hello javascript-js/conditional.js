928c7bfa5fb50c5376d1928d635b6fe37d777a37
Diptesh8 <shiva14420065@gmail.com>
2026-09-05 15:16:10 +0530

    added new line  code

--- Hello javascript-js/conditional.js
+++ Hello javascript-js/conditional.js
@@ -1,1 +1,13 @@
 console.log(7 > 3);
+console.log(5 < 6);
+if(13 > 12){
+    console.log("i have lot of million dolar income idea");
+}
+
+var briyaniPrice = 200;
+if(briyaniPrice < 240){
+    console.log("mama give me briyani");
+}
+else {
+    console.log("i eat chowmin");
+}
