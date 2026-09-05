0987068dd54f318efe4ecaad4766dc7fcb618c10
Diptesh8 <shiva14420065@gmail.com>
2026-09-05 15:29:41 +0530

    add new code of if and else

--- Hello javascript-js/conditional.js
+++ Hello javascript-js/conditional.js
@@ -11,3 +11,11 @@ if(briyaniPrice < 240){
 else {
     console.log("i eat chowmin");
 }
+const salary = 25000;
+const height = 6;
+if(salary > 20000 || height > 7){
+    console.log("aso baba kubul tomar biye hoobe ");
+}
+else{
+    console.log("baba ei jonme tomar r amr mayer sathe biye holo na");
+}
