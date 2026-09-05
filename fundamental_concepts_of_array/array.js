71b9aa27939b6101996d3406b87c9ef2906f43ef
Diptesh8 <shiva14420065@gmail.com>
2026-09-05 21:28:02 +0530

    added push and pop

--- fundamental_concepts_of_array/array.js
+++ fundamental_concepts_of_array/array.js
@@ -1,3 +1,4 @@
 const numbers = [12, 34, 6, 54, 59];
 const friends = ['abull', 'habul', 'dabul','cabul'];
-console.log(friends.length);
+numbers[1] = 'rajesh';
+console.log(friends);
---
+++ fundamental_concepts_of_array/push-pop.js
@@ -0,0 +1,5 @@
+const numbers = [12, 45, 7, 9, 34];
+console.log(numbers);
+// numbers.push(88);
+numbers.pop(777);
+console.log(numbers);
