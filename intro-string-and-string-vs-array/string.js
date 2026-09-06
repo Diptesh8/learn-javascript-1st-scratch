5d0b6b7489fb532f21c8b915bbe6b5af8c1a945e
Diptesh8 <shiva14420065@gmail.com>
2026-09-06 16:05:00 +0530

    added trim

---
+++ intro-string-and-string-vs-array/string.js
@@ -0,0 +1,19 @@
+const country ='India';
+const name ="Diptesh";
+const school =`ichapur High School`;
+console.log(school.length);
+console.log(school.toLowerCase());
+
+console.log(school.toUpperCase());
+console.log(typeof country);
+console.log(typeof name);
+
+// hoe to implement trim in string ---------------
+const life ="water";
+const jibon ="  water";
+if(life.trim === jibon.trim){
+    console.log("ami pani khabo");
+}
+else{
+    console.log("aj amr r pani khaua holo na");
+}
