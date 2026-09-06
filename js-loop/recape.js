b41dc46e7dec0e857e7fe3e2a24b3a412151a735
Diptesh8 <shiva14420065@gmail.com>
2026-09-06 08:48:52 +0530

    added numbers of array

---
+++ js-loop/recape.js
@@ -0,0 +1,8 @@
+const numbers = [12, 13, 14, 75, 9]
+for(const mun of numbers){
+    console.log(mun);
+}
+const fruits = ["apple" ,"banana", "pineapple","jackfruit"];
+for(const fruit of fruits){
+    console.log(fruit);
+}
