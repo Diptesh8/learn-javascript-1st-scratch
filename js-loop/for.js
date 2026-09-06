f1115a27d4203a28677e1ee38e3251200283fdbe
Diptesh8 <shiva14420065@gmail.com>
2026-09-06 13:28:04 +0530

    added  modulus condition usinf if

--- js-loop/for.js
+++ js-loop/for.js
@@ -6,3 +6,20 @@ for(let num =20 ; num >= 0; num--){
     console.log(num);
 }
 console.log("sum of numbers from1 to 20 is :-",sum);
+
+// find 1 to 30 all even number-----------------------
+
+
+// for(let i = 1 ; i<=30;i++){
+//      if(i%2==!0){
+// console.log(i);
+//      }
+    
+// }
+
+// find the number 1 to 30 division 3 and 5----------
+for(let num =1 ;num <= 30 ;num++){
+    if(num%3===0 && num%5===0){
+        console.log(num);
+    }
+}
