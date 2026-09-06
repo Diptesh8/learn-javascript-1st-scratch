8c8e01b18f2546381a657849e59cc6e46d32c5cc
Diptesh8 <shiva14420065@gmail.com>
2026-09-06 09:24:19 +0530

    add while.js file

---
+++ js-loop/while.js
@@ -0,0 +1,14 @@
+// learn while.loop---------------
+// let num = 0;
+// while(num < 5){
+//     console.log("amim vat khabo");
+//     num++;
+// }
+let num = 1;
+let sum =0;
+while(num <= 12){
+    console.log(num);
+    sum = sum + num ;
+    console.log("numbers of sum :-", sum);
+    num++;
+}
