8789858121b6add8d04a444c8a100ac5da274bf1
Diptesh8 <shiva14420065@gmail.com>
2026-09-06 13:53:29 +0530

    added  break file

--- js-loop/for.js
+++ js-loop/for.js
@@ -1,11 +1,11 @@
-let sum = 0 ;
-let num ;
+// let sum = 0 ;
+// let num ;
 
-for(let num =20 ; num >= 0; num--){
-    sum = sum + num;
-    console.log(num);
-}
-console.log("sum of numbers from1 to 20 is :-",sum);
+// for(let num =20 ; num >= 0; num--){
+//     sum = sum + num;
+//     console.log(num);
+// }
+// console.log("sum of numbers from1 to 20 is :-",sum);
 
 // find 1 to 30 all even number-----------------------
 
@@ -18,8 +18,18 @@ console.log("sum of numbers from1 to 20 is :-",sum);
 // }
 
 // find the number 1 to 30 division 3 and 5----------
-for(let num =1 ;num <= 30 ;num++){
-    if(num%3===0 && num%5===0){
-        console.log(num);
+// for(let num =1 ;num <= 30 ;num++){
+//     if(num%3===0 && num%5===0){
+//         console.log(num);
+//     }
+// }
+
+// give me the sum of numbers from 1 to 20 that devisable by 3----------------------
+let total = 0;
+for(let i =1 ;i<=20 ;i++){
+    if(i % 3 ===0){
+        console.log(i);
+        total =  total + i ;
+        console.log("sum of total ",total);
     }
 }
