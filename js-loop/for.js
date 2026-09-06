09978c4d670adf6774ebed400b1111d6cbecc89a
Diptesh8 <shiva14420065@gmail.com>
2026-09-06 12:55:59 +0530

    added new for loop decrement

--- js-loop/for.js
+++ js-loop/for.js
@@ -1,3 +1,8 @@
-for(let num =1 ; num <= 5; num++){
+let sum = 0 ;
+let num ;
+
+for(let num =20 ; num >= 0; num--){
+    sum = sum + num;
     console.log(num);
 }
+console.log("sum of numbers from1 to 20 is :-",sum);
