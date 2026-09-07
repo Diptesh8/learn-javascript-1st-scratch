5e95ef8b8bf95ca921551a21c2a3f71ce385496e
Diptesh8 <shiva14420065@gmail.com>
2026-09-07 14:20:26 +0530

    added slice working

---
+++ intro-string-and-string-vs-array/slice.js
@@ -0,0 +1,9 @@
+// const  address = "wheremykinghere";
+// const part = address.slice(7,11);
+// console.log(part);
+// const name = "DIprajaraju";
+// const vag = name.slice(3,7);
+// console.log(vag);
+
+const sentence = " I am a good person and hardworking .";
+console.log(sentence.split("h"));
