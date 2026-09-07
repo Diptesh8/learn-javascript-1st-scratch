56f104c6d8b374ef1a41d8094800adcfb6526a19
Diptesh8 <shiva14420065@gmail.com>
2026-09-07 17:41:03 +0530

    added new dot  noation

---
+++ intro-string-and-string-vs-array/object.js
@@ -0,0 +1,14 @@
+const service = {
+    type: "cleaning",
+
+    salary: 12000,
+
+    color: "green",
+
+    name: "orientaion"
+
+}
+console.log(service);
+console.log(service.color)
+const money = service.salary;
+console.log(money)
