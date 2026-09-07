61277c8cf1d077af72de79677bca8b25bf23b059
Diptesh8 <shiva14420065@gmail.com>
2026-09-07 17:10:25 +0530

    added new short reverse use

--- intro-string-and-string-vs-array/reverse.js
+++ intro-string-and-string-vs-array/reverse.js
@@ -1,8 +1,12 @@
-const sentence = "I am learning Web Dev."
-// const result =".veD beW gninrael ma I"
-let reverse =""
-for(const letter of sentence){
-    // console.log(letter);
-    reverse = letter + reverse;
-}
+// const sentence = "I am learning Web Dev."
+// // const result =".veD beW gninrael ma I"
+// let reverse =""
+// for(const letter of sentence){
+//     // console.log(letter);
+//     reverse = letter + reverse;
+
+// console.log(reverse);
+
+const names = "I am powerful and rech poople"
+const reverse = names.split("").reverse().join("");
 console.log(reverse);
