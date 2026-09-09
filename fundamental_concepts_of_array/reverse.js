c06fb2cb3f643571f3ef01067b94fa51c176c9ff
Diptesh8 <shiva14420065@gmail.com>
2026-09-09 17:50:19 +0530

    added reverse array using for loop

--- fundamental_concepts_of_array/reverse.js
+++ fundamental_concepts_of_array/reverse.js
@@ -1,3 +1,10 @@
 const names =["bill", "elon", "mark","waren","Diptesh"];
-const reversed =names.reverse();
-console.log(reversed)
+// const reversed =names.reverse();
+// console.log(reversed)
+
+// reverse using for loop -------------
+
+for(let i = names.length-1 ; i >= 0;i--){
+    const name = names[i];
+    console.log(name)
+}
