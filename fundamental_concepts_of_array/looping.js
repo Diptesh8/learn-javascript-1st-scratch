6267ef2acd3e84b36601b1401e10c31c1a19d1b1
Diptesh8 <shiva14420065@gmail.com>
2026-09-09 17:29:01 +0530

    added new file reverse.js

--- fundamental_concepts_of_array/looping.js
+++ fundamental_concepts_of_array/looping.js
@@ -1,9 +1,9 @@
-const friends = ["elon", "mark","waren","billgate"];
+const friends = ["elon", "mark","waren","billgate","Diptesh"];
 for(const friend of friends){
     // console.log(friend);
 }
 
 // here same output using for-loop-------------
-for(let i = 0; i < 5;i++){
+for(let i = 0; i < friends.length;i++){
     console.log(friends[i])
 }
