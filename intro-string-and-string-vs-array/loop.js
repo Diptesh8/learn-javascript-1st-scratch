1586c7b2acb0c0ca4559487ae6972caa2b96f244
Diptesh8 <shiva14420065@gmail.com>
2026-09-09 16:32:19 +0530

    addded loop an object see the value of result

---
+++ intro-string-and-string-vs-array/loop.js
@@ -0,0 +1,11 @@
+const mobile = {
+    brand: "samsung",
+    price: 12000,
+    color: "blue",
+    camara: "12mp"
+}
+for(const op in mobile){
+    console.log(op)
+    console.log(mobile[op])
+
+}
