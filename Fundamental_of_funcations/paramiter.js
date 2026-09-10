bf86dc398b459d18cb0e1b2bf576f860e07e33ff
Diptesh8 <shiva14420065@gmail.com>
2026-09-10 20:49:59 +0530

    added all unsaved file

--- Fundamental_of_funcations/paramiter.js
+++ Fundamental_of_funcations/paramiter.js
@@ -6,3 +6,12 @@ function squre(number){
 squre(5);
 
 squre(10);
+
+// addition using funcation-------------------
+
+function add(num1,num2,num3){
+const sum = num1 + num2 + num3;
+console.log("number1 and number2 and number3="num2+num1+num3=sum);
+
+}
+add(13,12,10);
