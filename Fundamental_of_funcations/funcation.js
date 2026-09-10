9a3252648a8762a629f324bf3594ae76d9af6a02
Diptesh8 <shiva14420065@gmail.com>
2026-09-10 20:15:06 +0530

    added all new file and folder

---
+++ Fundamental_of_funcations/funcation.js
@@ -0,0 +1,9 @@
+// funcation decleared and  call---------------------
+function  brushTeeth(){
+    console.log("first pick up the brush");
+    console.log("pick up the colgeet and over the brush");
+    console.log("and the last inside the mouth and sparke the teeth");
+}
+
+brushTeeth();
+brushTeeth();
