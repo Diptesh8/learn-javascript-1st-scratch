6d9397ff77039a35b434a479de184fe2fd0ee0f0
Diptesh8 <shiva14420065@gmail.com>
2026-09-10 08:21:01 +0530

    added reverse using unshift

--- fundamental_concepts_of_array/reverse.js
+++ fundamental_concepts_of_array/reverse.js
@@ -6,5 +6,13 @@ const names =["bill", "elon", "mark","waren","Diptesh"];
 
 for(let i = names.length-1 ; i >= 0;i--){
     const name = names[i];
-    console.log(name)
+    // console.log(name)
 }
+
+// reverse array using unshift ---------------------------
+const rev_numb =[];
+for(const name of names){
+    console.log(name);
+    rev_numb.unshift(name)
+}
+console.log(rev_numb);
