4628bf917e4be7c57a05e42633220fb5ab390ab3
Diptesh8 <shiva14420065@gmail.com>
2026-09-11 13:06:05 +0530

    added function odd even  trick using return

--- Fundamental_of_funcations/recape.js
+++ Fundamental_of_funcations/recape.js
@@ -1,7 +1,18 @@
 function number(num1,num2){
-    const total = num1 * num2 ;
-    return total;
-    console.log(total);
+    return num1 * num2 ;
 }
 
-number(4,5);
+ const mot =number(9,5);
+console.log(mot);
+
+function isEven(number) {
+    if (number % 2 ===0){
+        return true ;
+    }
+    else{
+        return false;
+    }
+    
+}
+console.log(isEven(5));
+console.log(isEven(34));
