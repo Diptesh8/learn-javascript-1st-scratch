0d80f7cb7d136323796cb010b721b20030858154
Diptesh8 <shiva14420065@gmail.com>
2026-09-11 20:39:31 +0530

    added new a rray  single number and sum

--- Fundamental_of_funcations/array_operation.js
+++ Fundamental_of_funcations/array_operation.js
@@ -1,11 +1,14 @@
 // objective : write a function to give me the sum of all numbers in an array-----------------------------
 
-function sumOfNumbers(){
-    for(const number of numbers){
-     console.log(number);
-    }
-   
-
+function sumOfNumbers(numbers){
+    let sum = 0;
+     for(const number of numbers){
+      console.log(number);
+      sum = sum + number;
+ }
+    
+    return sum;
 }
-const numbs = [4,6,8,9];
+const numbs = [4,6,8,9,6,8,5,4,9];
 const sum = sumOfNumbers(numbs);
+console.log("sum of number is",sum);
