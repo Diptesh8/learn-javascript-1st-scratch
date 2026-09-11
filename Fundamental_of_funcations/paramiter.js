6033d3058585f7cd25f0f4dbfe82e47a05e040dd
Diptesh8 <shiva14420065@gmail.com>
2026-09-11 12:33:33 +0530

    added all new file

--- Fundamental_of_funcations/paramiter.js
+++ Fundamental_of_funcations/paramiter.js
@@ -9,9 +9,9 @@ squre(10);
 
 // addition using funcation-------------------
 
-function add(num1,num2,num3){
-const sum = num1 + num2 + num3;
-console.log("number1 and number2 and number3="num2+num1+num3=sum);
+function add(number1,num2,num3){
+const sum = number1 + num2 + num3;
+console.log("number1 and number2 and number3=",sum);
 
 }
 add(13,12,10);
