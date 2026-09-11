4043d342ad80b4907df09daf995f3c358a21fc21
Diptesh8 <shiva14420065@gmail.com>
2026-09-11 20:26:51 +0530

    added all new file

---
+++ Fundamental_of_funcations/array_operation.js
@@ -0,0 +1,11 @@
+// objective : write a function to give me the sum of all numbers in an array-----------------------------
+
+function sumOfNumbers(){
+    for(const number of numbers){
+     console.log(number);
+    }
+   
+
+}
+const numbs = [4,6,8,9];
+const sum = sumOfNumbers(numbs);
