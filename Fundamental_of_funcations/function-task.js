c5cef5579cd4f79426131e03d0e8b0b3f0b5163c
Diptesh8 <shiva14420065@gmail.com>
2026-09-12 08:07:28 +0530

    added multiple function using return

---
+++ Fundamental_of_funcations/function-task.js
@@ -0,0 +1,8 @@
+// take four parameters.multiply the four numbers and then return the result.--------------------------
+
+function mul(num1,num2,num3,num4){
+   const multiple = num1 * num2 * num3 * num4 ;
+   return multiple;
+}
+const ans = mul(3,5,2,3);
+console.log(ans);
