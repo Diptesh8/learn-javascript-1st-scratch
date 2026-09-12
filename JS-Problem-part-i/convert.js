9de7e9755faa7845e3b107c7ec4054f035b34933
Diptesh8 <shiva14420065@gmail.com>
2026-09-12 15:48:27 +0530

    added inchi to feet using function

--- JS-Problem-part-i/convert.js
+++ JS-Problem-part-i/convert.js
@@ -1,9 +1,12 @@
 // inchi to feet using javascript-------------------
 function inchiFeet(inch){
-    const feet =inchi/12;
-    return feet;
+    const feet = inch / 12;
+    const feetnumber =parseInt(feet)
+    const feetInchi =inch % 12;
+    const result =feetnumber + " feet" + feetInchi + " inchi"
+    return result; 
 
 }
-const suvoHeight= "39";
+const suvoHeight= "53";
 const answerFeet= inchiFeet(suvoHeight);
-console.log(answer);
+console.log(answerFeet);
