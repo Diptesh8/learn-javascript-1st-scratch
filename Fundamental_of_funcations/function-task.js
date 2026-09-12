c78b1820df2032a16089923f12f0c8cacfaba947
Diptesh8 <shiva14420065@gmail.com>
2026-09-12 08:19:35 +0530

    added  a number is odd mul 2 and else even so devide 2

--- Fundamental_of_funcations/function-task.js
+++ Fundamental_of_funcations/function-task.js
@@ -4,5 +4,18 @@ function mul(num1,num2,num3,num4){
    const multiple = num1 * num2 * num3 * num4 ;
    return multiple;
 }
-const ans = mul(3,5,2,3);
-console.log(ans);
+// const ans = mul(3,5,2,3);
+// console.log(ans);
+
+// Task-2 take a number if the number is odd multiply iy by 2 and return the result.if the number is even divide it by 2 and return the result
+
+function number(num1){
+    if(num1 % 2===1){
+      return num1 * 2;
+    }
+    else{
+      return  num1 / 2;
+    }
+}
+const num= number(8);
+console.log(num);
