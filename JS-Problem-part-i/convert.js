596be747d6c548dcb1457951041cfbb11c0c40ea
Diptesh8 <shiva14420065@gmail.com>
2026-09-12 15:34:39 +0530

    added all new file

---
+++ JS-Problem-part-i/convert.js
@@ -0,0 +1,9 @@
+// inchi to feet using javascript-------------------
+function inchiFeet(inch){
+    const feet =inchi/12;
+    return feet;
+
+}
+const suvoHeight= "39";
+const answerFeet= inchiFeet(suvoHeight);
+console.log(answer);
