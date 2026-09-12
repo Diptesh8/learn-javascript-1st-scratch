0383c5a8a4c2cd127431cc13d998d435feb0a44a
Diptesh8 <shiva14420065@gmail.com>
2026-09-12 20:54:46 +0530

    added string duplicate

--- JS-Problem-part-i/no-duplicate.js
+++ JS-Problem-part-i/no-duplicate.js
@@ -12,5 +12,21 @@ function unik(array){
 }
 
 const unique =[15,3,6,15,8,6,4,9,3,6,5,12,43,2];
-const answer = unik(unique);
-console.log(answer);
+// const answer = unik(unique);
+// console.log(answer);
+
+// find the duplicate in a string of array and  using function and array-----------
+
+function duplicate(string){
+    const newArray=[];
+    for(const name of string){
+        if(newArray.includes(name) === false){
+            newArray.push(name);
+        }
+    }
+// console.log(string);
+return newArray;
+}
+const array =["dip","raju","raja","king","dip","raja","gyan","nobita","doremon","king"];
+const answer= duplicate(array);
+console.log(answer)
