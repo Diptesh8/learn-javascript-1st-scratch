7793129146255c1917b1556af8b042f8a1109b55
Diptesh8 <shiva14420065@gmail.com>
2026-09-13 11:16:51 +0530

    added new code of three max

--- js-problem-part-ii/max.js
+++ js-problem-part-ii/max.js
@@ -1,4 +1,4 @@
-const max(num1, num2){
+function max(num1, num2){
     if(num1>num2){
         console.log(num1)
     }
@@ -9,4 +9,22 @@ const max(num1, num2){
 console
 }
 
-max(45,9);
+max(45,989);
+
+// find the max number in 3 numbers---------------------------------
+
+function maxmium(nam1,nam2,nam3){
+    if(nam1 > nam2 && nam1 >nam3){
+        return nam1;
+    }
+    else if(nam2 > nam1 && nam2 > nam3){
+        return nam2;
+    }
+    else{
+        return nam3;
+    }
+
+}
+// const number =[32,65,51];
+const answer=maxmium(65,98,436);
+console.log("the maximum number is=",answer);
