8f366e95599694a48daf953b6bd43057bd7aaf8b
Diptesh8 <shiva14420065@gmail.com>
2026-09-13 20:35:48 +0530

    added new file

--- js-problem-part-ii/total.js
+++ js-problem-part-ii/total.js
@@ -1,18 +1,19 @@
 const product = [
-    {name: "shampoo", price:300 },
-    {name: "pant", price:1200},
-    {name: "shirt", price:2000},
-    {name: "protin", price:1300},
+    {name: "shampoo", price:300, quainty :3 },
+    {name: "pant", price:1200, quainty :6},
+    {name: "shirt", price:2000, quainty :4},
+    {name: "protin", price:1300, quainty :5},
 ]
 
 function totalPrice(costs){
     let sum = 0;
+    let total =0;
     for(const cost of costs){
-        sum = sum + cost.price
-
+        sum =  cost.price * cost.quainty;
+        total = total + sum;
+    
     }
-    console.log(costs);
-    return sum;
+    return total;
 
 }
 
