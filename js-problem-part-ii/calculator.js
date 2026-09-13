e7e4cf23ee390c347ae88b95c1eb29f6e8788ac7
Diptesh8 <shiva14420065@gmail.com>
2026-09-13 22:59:25 +0530

    added calculatot using function

---
+++ js-problem-part-ii/calculator.js
@@ -0,0 +1,37 @@
+function addition(num1,num2){
+    return num1 + num2;
+}
+function subtract(num1,num2){
+    return num1 - num2;
+}
+function multip(num1,num2){
+    return num1 * num2;
+}
+function devide(num1,num2){
+    return num1 / num2;
+}
+
+function calculator(a ,b,operator){
+    if(operator === "addition"){
+         return addition(a,b)
+    }
+    else if( operator === "subtract"){
+        const result= subtract(a,b);
+        return result;
+    }
+    else if( operator === "multip"){
+        const result= multip(a,b);
+        return result;
+    }
+    else if( operator === "devide"){
+       const result= devide(a,b);
+       return result;
+    }
+    else{
+        return "only 'addition','subtract','multip','devide'"
+    }
+           
+}
+
+const rehave =calculator(5,6,"devide");
+console.log(rehave);
