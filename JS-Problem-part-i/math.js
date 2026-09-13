ac09edc2a925f28a7ca084f07d91bcc89f78a4f3
Diptesh8 <shiva14420065@gmail.com>
2026-09-13 09:34:01 +0530

    added minimum number in array

---
+++ JS-Problem-part-i/math.js
@@ -0,0 +1,9 @@
+function minimum(number){
+    const min = Math.min(...number)
+    return min ;
+
+}
+// const name = [12,5,7,4,86,4,3];
+// const value = minimum(name);
+const value =minimum([4,5,2,7,8,32,1,-8,34,-456,]);
+console.log(value);
