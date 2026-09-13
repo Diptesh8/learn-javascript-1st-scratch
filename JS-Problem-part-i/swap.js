beb190a7d28750ffd7afa19174d19688768c98b2
Diptesh8 <shiva14420065@gmail.com>
2026-09-13 10:26:38 +0530

    added  number change code

---
+++ JS-Problem-part-i/swap.js
@@ -0,0 +1,13 @@
+let a =5;
+let b =7;
+const temp = 5;
+a = b ;
+b = temp;
+console.log(a,b);
+
+//  numbber change  using distructing---------------------------
+let x  = 8;
+let y = 39;
+console.log(x,y);
+[x, y] = [y, x];
+console.log(x,y);
