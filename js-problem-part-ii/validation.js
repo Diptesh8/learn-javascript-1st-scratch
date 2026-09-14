6cabd61c6577bb1698bb10b8d908a12ef327fe91
Diptesh8 <shiva14420065@gmail.com>
2026-09-14 21:11:51 +0530

    added unexpected funcation how to handle and error messege show

---
+++ js-problem-part-ii/validation.js
@@ -0,0 +1,11 @@
+function error(num1 ,num2){
+  if(typeof num1 !== "number" || typeof num2 !== "number"){
+    return "enter a valid number";
+  }
+    console.log(num1);
+const mul = num1 *num2;
+return mul;
+}
+
+const number = error(dh,"vdgg");
+console.log(number);
