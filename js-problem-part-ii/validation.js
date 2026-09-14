1c204b24db977eec491c0823f028d45be07660e2
Diptesh8 <shiva14420065@gmail.com>
2026-09-14 21:32:52 +0530

    added  new string validation function and error messege prient

--- js-problem-part-ii/validation.js
+++ js-problem-part-ii/validation.js
@@ -7,5 +7,24 @@ const mul = num1 *num2;
 return mul;
 }
 
-const number = error(dh,"vdgg");
-console.log(number);
+// const number = error(dh,"vdgg");
+// console.log(number);
+
+// enter a valid  name otherwise show the error output------------------------
+
+function fullname(first,second){
+    if(typeof first !== "string" ){
+        return"error please enter a valid string"
+    }
+    else if(typeof second !== "string"){
+        return"error please enter a valid string"
+    }
+        
+    
+    const attach =first + " " + second;
+    return attach;
+
+}
+
+const names= fullname("batash", "raja");
+console.log(names);
