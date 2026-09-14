5a80e616c0b25a9a6d47ce2b3551bad4f48c7a6d
Diptesh8 <shiva14420065@gmail.com>
2026-09-15 00:48:02 +0530

    added new number array function

--- assignment-folder/assigment.js
+++ assignment-folder/assigment.js
@@ -17,12 +17,35 @@ function calculateMoney(ticket){
 function checkName(name){
     
  let lastLetter= name[name.length-1];
- if(lastLetter === "a" || lastLetter === "y" || lastLetter === "i" || lastLetter === "e" || lastLetter === "o" || lastLetter === "u" || lastLetter === "w"){
+ if(typeof name !== "string"){
+    return "enter a valid string"
+ }
+ else if(lastLetter === "a" || lastLetter === "y" || lastLetter === "i" || lastLetter === "e" || lastLetter === "o" || lastLetter === "u" || lastLetter === "w"){
     return "Good name";
  }
  else{
     return "Bad name";
  }
 }
-const output= checkName("raji");
-console.log(output);
+// const output= checkName("raja");
+// console.log(output);
+
+// problem3:= Virus  in my  Array and filter it only number are allowed using function and return keyword ------------------------------
+
+function deleteInvalids(array){
+    let finalArray=[];
+    if(Array.isArray(array) !== true){
+        console.log("input a valid array do not try anything");
+        return;
+    }
+for(const num of array){
+  if(typeof num === "number" && !isNaN(num)){
+  finalArray.push(num);
+//   console.log(finalArray);
+  }
+}
+return finalArray;
+}
+const outpu= deleteInvalids([18,13,-7,NaN,"unvalid",87]);
+console.log(outpu);
+
