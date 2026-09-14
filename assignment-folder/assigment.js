b8732e1f8932498b11e3b397f55256115dc5a9c6
Diptesh8 <shiva14420065@gmail.com>
2026-09-14 23:53:42 +0530

    added a function define a name is good or bad

--- assignment-folder/assigment.js
+++ assignment-folder/assigment.js
@@ -1,4 +1,4 @@
-// help to Zoo Maneger program ---------------
+//Problem-1:= help to Zoo Maneger program ---------------
 
 function calculateMoney(ticket){
     if(ticket <= 0){
@@ -9,5 +9,20 @@ function calculateMoney(ticket){
     const profit = ticketSell - dailyExpenses ;
     return profit;
 }
-const output =calculateMoney(10);
+// const output =calculateMoney(10);
+// console.log(output);
+
+//Problem2:= Check  a Name is Good or Bad usiging functionand return keyword----------------------------
+
+function checkName(name){
+    
+ let lastLetter= name[name.length-1];
+ if(lastLetter === "a" || lastLetter === "y" || lastLetter === "i" || lastLetter === "e" || lastLetter === "o" || lastLetter === "u" || lastLetter === "w"){
+    return "Good name";
+ }
+ else{
+    return "Bad name";
+ }
+}
+const output= checkName("raji");
 console.log(output);
