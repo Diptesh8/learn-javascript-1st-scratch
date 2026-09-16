d0a8fce778a34ccf3fb760b4bc064345bc3d345b
Diptesh8 <shiva14420065@gmail.com>
2026-09-16 20:44:50 +0530

    added new list add

---
+++ tour-of-dom(Document object Model)/append.js
@@ -0,0 +1,12 @@
+
+// 1. where to add
+
+const placeslist = document.getElementById("places-list");
+
+// 2. what to be added
+const li =document.createElement("li");
+li.innerText= "pahartoli bon";
+
+// 3. add the child
+
+placeslist.appendChild(li);
