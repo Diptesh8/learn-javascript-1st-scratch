aaa1614834723cdba98646fe6a8af8afeade536c
Diptesh8 <shiva14420065@gmail.com>
2026-09-16 21:22:15 +0530

    added inner html part

--- tour-of-dom(Document object Model)/append.js
+++ tour-of-dom(Document object Model)/append.js
@@ -19,8 +19,33 @@ const section =document.createElement("section");
 const h1 =document.createElement("h1");
 h1.innerText ="my hobby list";
 section.appendChild(h1);
+const ul = document.createElement("ul");
+const li1 = document.createElement("li");
+li1.innerText= "programming";
+ul.appendChild(li1);
+
+const li2 = document.createElement("li");
+li1.innerText= "gym";
+ul.appendChild(li2);
+
+const li3 = document.createElement("li");
+li1.innerText= "dancing";
+ul.appendChild(li3);
+
+section.appendChild(ul);
 
 mainContainer.appendChild(section);
 
-const ul = document.createElement("ul");
-const li = document.createElement("li");
+// set inner html--------
+
+const sectionDress = document.createElement("section");
+sectionDress.innerHTML=`
+<h1> My Dress Section </h1>
+<ul>
+<li>jinse</li>
+<li>baggy</li>
+<li>shirt</li>
+<li>hoddy</li>
+</ul>
+`
+mainContainer.appendChild(sectionDress)
