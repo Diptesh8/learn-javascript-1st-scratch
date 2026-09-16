f576ebc4f8ecf7695f52fb269b965555977c355d
Diptesh8 <shiva14420065@gmail.com>
2026-09-16 17:45:22 +0530

    added new line radious code

--- tour-of-dom(Document object Model)/style.js
+++ tour-of-dom(Document object Model)/style.js
@@ -3,4 +3,5 @@ const sections = document.querySelectorAll("section");
 for(const section of sections){
     section.style.border = "2px solid blue";
     section.style.marginBottom ="8px";
+    section.style.borderRadius = "15px";
 }
