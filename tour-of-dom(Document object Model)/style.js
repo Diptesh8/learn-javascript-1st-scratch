ca24ba129fa4f2e6e5959518a507a72cd689362d
Diptesh8 <shiva14420065@gmail.com>
2026-09-16 17:42:35 +0530

    added new javascript code

---
+++ tour-of-dom(Document object Model)/style.js
@@ -0,0 +1,6 @@
+const sections = document.querySelectorAll("section");
+// console.log(sections);
+for(const section of sections){
+    section.style.border = "2px solid blue";
+    section.style.marginBottom ="8px";
+}
