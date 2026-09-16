45f4e0c15d87dce6a8332ff06a3f6ace10816abb
Diptesh8 <shiva14420065@gmail.com>
2026-09-16 21:04:40 +0530

    added new line of code

--- tour-of-dom(Document object Model)/append.js
+++ tour-of-dom(Document object Model)/append.js
@@ -10,3 +10,17 @@ li.innerText= "pahartoli bon";
 // 3. add the child
 
 placeslist.appendChild(li);
+
+// 
+
+const mainContainer =document.getElementById("main");
+
+const section =document.createElement("section");
+const h1 =document.createElement("h1");
+h1.innerText ="my hobby list";
+section.appendChild(h1);
+
+mainContainer.appendChild(section);
+
+const ul = document.createElement("ul");
+const li = document.createElement("li");
